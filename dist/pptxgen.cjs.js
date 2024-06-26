c5eba52ba60c42de0b4746ef54d65ddff3c5ee91
Mike Hollibaugh <mike.hollibaugh@pitchly.com>
2024-06-26 08:40:18 -0700

    console log

--- dist/pptxgen.cjs.js
+++ dist/pptxgen.cjs.js
@@ -2431,6 +2431,7 @@ function genXmlBodyProperties(slideObject) {
  * @returns XML containing the param object's text and formatting
  */
 function genXmlTextBody(slideObj) {
+  console.log('genXmlParagraphProperties');
     var opts = slideObj.options || {};
     var tmpTextObjects = [];
     var arrTextObjects = [];
