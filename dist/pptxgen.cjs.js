bfabc54da1891402d949225c0b63b4d68db83282
Mike Hollibaugh <mike.hollibaugh@pitchly.com>
2024-07-23 14:49:14 -0700

    remove logging

--- dist/pptxgen.cjs.js
+++ dist/pptxgen.cjs.js
@@ -1774,11 +1774,7 @@ function slideItemObjsToXml(slideItemObjs, slide) {
                 strSlideXml += '<p:pic>';
                 strSlideXml += '  <p:nvPicPr>';
                 let imageName = slideItemObj.options.shapeName ? slideItemObj.options.shapeName : "Object " + (idx + 1);
-                console.log('imageName', imageName);
-                console.log('slideItemObj', slideItemObj);
                 strSlideXml += '    <p:cNvPr id="' + (idx + 2) + '" name="'+ imageName + '" descr="' + encodeXmlEntities(slideItemObj.image) + '">';
-                //console.log('    <p:cNvPr id="' + (idx + 2) + '" name="Object ' + (idx + 1) + '" descr="' + encodeXmlEntities(slideItemObj.image) + '">');
-                //strSlideXml += '    <p:cNvPr id="' + (idx + 2) + '" name="Object ' + (idx + 1) + '" descr="' + encodeXmlEntities(slideItemObj.image) + '">';
               if (slideItemObj.hyperlink && slideItemObj.hyperlink.url)
                     strSlideXml +=
                         '<a:hlinkClick r:id="rId' +
